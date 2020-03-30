ef0334639a23be38a59214094a94186aa17b3860
ph4ntonn <ph4ntom11235@gmail.com>
2020-03-30 12:38:43 +0800

    Update version

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-const version = "1.2"
+const version = "1.5"
 
 //注意！需要编译agent模式的程序时，在20行的admin.Command前加上注释符号‘//’，去掉19行的注释符号, 并将上方import Stowaway/admin改为Stowaway/agent
 //注意！需要编译admin模式的程序时，在19行的agent.Command前加上注释符号‘//’，去掉20行的注释符号, 并将上方import Stowaway/agent改为Stowaway/admin
