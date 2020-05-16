504464a2a0b12f0348cc2a30c00e579cf8090acd
ph4ntom <ph4ntom11235@gmail.com>
2020-05-16 17:25:34 +0800

    XD

--- admin/command.go
+++ admin/command.go
@@ -3,7 +3,6 @@ package admin
 import (
 	"flag"
 	"log"
-	"os"
 
 	"Stowaway/utils"
 )
@@ -26,13 +25,11 @@ func ParseCommand() {
 	if Args.Listen != "" && Args.Connect == "" {
 		log.Printf("[*]Starting admin node on port %s\n", Args.Listen)
 	} else if Args.Connect != "" && Args.Listen != "" {
-		log.Println("[*]If you are using active connect mode, do not set -l option")
-		os.Exit(0)
+		log.Fatalln("[*]If you are using active connect mode, do not set -l option")
 	} else if Args.Connect != "" && Args.Listen == "" {
 		log.Println("[*]Trying to connect startnode actively...")
 	} else {
-		log.Println("Bad format! See readme!")
-		os.Exit(0)
+		log.Fatalln("Bad format! See readme!")
 	}
 	NewAdmin(Args)
 }
--- agent/command.go
+++ agent/command.go
@@ -3,7 +3,6 @@ package agent
 import (
 	"flag"
 	"log"
-	"os"
 
 	"Stowaway/utils"
 )
@@ -32,32 +31,25 @@ func ParseCommand() {
 	if Args.Listen != "" && Args.Reverse && Args.Monitor == "" {
 		log.Printf("Starting agent node on port %s passively\n", Args.Listen)
 	} else if Args.Listen != "" && Args.Reverse && Args.Monitor != "" {
-		log.Println("If you want to start node passively,do not set the -m option")
-		os.Exit(0)
+		log.Fatalln("If you want to start node passively,do not set the -m option")
 	} else if Args.Listen != "" && !Args.Reverse && Args.Monitor == "" && Args.ReusePort == "" {
-		log.Println("You should set the -m option!")
-		os.Exit(0)
+		log.Fatalln("You should set the -m option!")
 	} else if !Args.Reverse && Args.Monitor != "" {
 		log.Println("Node starting......")
 	} else if Args.Reconnect != "" && !Args.IsStartNode {
-		log.Println("Do not set the --reconnect option on simple node")
-		os.Exit(0)
+		log.Fatalln("Do not set the --reconnect option on simple node")
 	} else if (Args.ReusePort != "" || Args.ReuseHost != "") && (Args.Monitor != "") {
-		log.Println("Choose one from (--report,--rehost) and -m")
-		os.Exit(0)
+		log.Fatalln("Choose one from (--report,--rehost) and -m")
 	} else if Args.ReusePort != "" && Args.ReuseHost == "" && Args.Listen == "" {
-		log.Println("If you want to reuse port through iptable method,you should set --report and -l simultaneously,or if you want to use SO_REUSE method,you should set --report and --rehost instead")
-		os.Exit(0)
+		log.Fatalln("If you want to reuse port through iptable method,you should set --report and -l simultaneously,or if you want to use SO_REUSE method,you should set --report and --rehost instead")
 	} else if Args.ReusePort != "" && Args.ReuseHost != "" && Args.Listen != "" {
-		log.Println("Should be report+rehost or report+listen")
-		os.Exit(0)
+		log.Fatalln("Should be report+rehost or report+listen")
 	} else if (Args.ReusePort != "" && Args.ReuseHost != "") && (Args.Listen == "" && Args.Monitor == "") {
 		log.Printf("Starting agent node reusing port %s \n", Args.ReusePort)
 	} else if Args.ReusePort != "" && Args.Listen != "" && Args.ReuseHost == "" && Args.Monitor == "" {
 		log.Printf("Now agent node is listening on port %s and reusing port %s", Args.Listen, Args.ReusePort)
 	} else {
-		log.Println("Bad format! See readme!")
-		os.Exit(0)
+		log.Fatalln("Bad format! See readme!")
 	}
 	NewAgent(Args)
 }
